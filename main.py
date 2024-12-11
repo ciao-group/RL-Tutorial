c1168ebb4af5f683393f59b8065df1a38f3bf61c
Martin Lorenz <25765799+martinoz1024@users.noreply.github.com>
2024-12-11 17:21:55 +0100

    feat: prefill main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 import Environments.findTargetEnv
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 
 # Press the green button in the gutter to run the script.
@@ -39,16 +39,32 @@ def demo_trainaed_model(model_path:str):
 
 
 if __name__ == '__main__':
-
     # create env
-
+    env = gym.make("FindTargetEnv-v0", render_mode="rgb_array", size=5)
     # create model
-
+    ALGO = PPO
+    model = ALGO("MlpPolicy", env=env, verbose=1)
     # train model
-
+    model.learn(total_timesteps=5e5, progress_bar=True)
     # save model
-
+    model_path = "FindTarget_5e4"
+    model.save(model_path)
     # demo model
 
-    pass
+    env = gym.make("FindTargetEnv-v0", render_mode="human", size=5)
+    model = ALGO.load(model_path, env)
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+
+    for _ in range(10000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, done, info = vec_env.step(action)
+
+        vec_env.render("human")
+
+        if done:
+            obs = vec_env.reset()
+
+    vec_env.close()
+    #pass
 
